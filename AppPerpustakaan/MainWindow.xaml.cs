e18ce251dcd098713fc68a5386bbc2154050a792
ocph23 <ocph23@gmail.com>
2024-09-30 22:17:57 +0900

    Add project files.

---
+++ AppPerpustakaan/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace AppPerpustakaan
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            GetDataFromExcel();
+
+
+        }
+
+        private void GetDataFromExcel()
+        {
+            try
+            {
+                //Lets open the existing excel file and read through its content . Open the excel using openxml sdk
+                using (SpreadsheetDocument doc = SpreadsheetDocument.Open("database1.xlsx", false))
+                {
+                    //create the object for workbook part  
+                    WorkbookPart workbookPart = doc.WorkbookPart;
+                    Sheets thesheetcollection = workbookPart.Workbook.GetFirstChild<Sheets>();
+                    List<Siswa> dataSiswa = new List<Siswa>();
+
+                    //using for each loop to get the sheet from the sheetcollection  
+                    foreach (Sheet thesheet in thesheetcollection)
+                    {
+                        //statement to get the worksheet object by using the sheet id  
+                        Worksheet theWorksheet = ((WorksheetPart)workbookPart.GetPartById(thesheet.Id)).Worksheet;
+
+                        SheetData thesheetdata = (SheetData)theWorksheet.GetFirstChild<SheetData>();
+                        foreach (Row thecurrentrow in thesheetdata)
+                        {
+                            var siswa = new Siswa();
+
+
+                            foreach (Cell thecurrentcell in thecurrentrow)
+                            {
+                                //statement to take the integer value  
+                                string currentcellvalue = string.Empty;
+                                if (thecurrentcell.DataType != null)
+                                {
+                                    if (thecurrentcell.DataType == CellValues.SharedString)
+                                    {
+
+                                        
+
+                                        int id;
+                                        if (Int32.TryParse(thecurrentcell.InnerText, out id))
+                                        {
+                                            SharedStringItem name = workbookPart.SharedStringTablePart.SharedStringTable.Elements<SharedStringItem>().ElementAt(0);
+                                            siswa.Nama = name?.Text.Text;
+
+                                            SharedStringItem nisn= workbookPart.SharedStringTablePart.SharedStringTable.Elements<SharedStringItem>().ElementAt(2);
+                                            siswa.NISN= nisn?.Text.Text;
+
+                                        }
+                                    }
+                                }
+                                else
+                                {
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
---
+++ AppPerpustakaan/Siswa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPerpustakaan
+{
+    public class Siswa
+    {
+        public string Nama { get; set; }
+        public string NISN{ get; set; }
+        public string TempatLahir{ get; set; }
+        public string TanggalLahir{ get; set; }
+        public string NIK{ get; set; }
+        public string Agama{ get; set; }
+        public string JenisKelamin { get; internal set; }
+    }
+}
